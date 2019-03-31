cd5d660afaf7bf191f81f8d50dee7064d3025bc9
Andy Miller <rhuk@mac.com>
2019-03-31 09:01:45 -0600

    fixes

--- plugins/prism-highlight/prism-highlight.php
+++ plugins/prism-highlight/prism-highlight.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Grav\Plugin;
 
 use \Grav\Common\Plugin;
@@ -7,55 +8,71 @@ use \Grav\Common\Page\Page;
 
 class PrismHighlightPlugin extends Plugin
 {
-  /**
-   * @return array
-   */
-  public static function getSubscribedEvents()
-  {
-    return [
-        'onPageInitialized' => ['onPageInitialized', 0]
-    ];
-  }
-
-  /**
-   * Initialize configuration
-   */
-  public function onPageInitialized()
-  {
-    if ($this->isAdmin()) {
-      $this->active = false;
-      return;
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPageInitialized' => ['onPageInitialized', 0]
+        ];
     }
 
-    $defaults = (array) $this->config->get('plugins.prism-highlight');
+    /**
+     * Initialize configuration
+     */
+    public function onPageInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
 
-    /** @var Page $page */
-    $page = $this->grav['page'];
-    if (isset($page->header()->prism)) {
-      $this->config->set('plugins.prism-highlight', array_merge($defaults, $page->header()->prism));
+        $defaults = (array)$this->config->get('plugins.prism-highlight');
+
+        /** @var Page $page */
+        $page = $this->grav['page'];
+        if (isset($page->header()->prism)) {
+            $this->config->set('plugins.prism-highlight', array_merge($defaults, $page->header()->prism));
+        }
+        if ($this->config->get('plugins.prism-highlight.enabled')) {
+            $this->enable([
+                'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+            ]);
+        }
+
     }
-    if ($this->config->get('plugins.prism-highlight.enabled')) {
-      $this->enable([
-          'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
-      ]);
+
+    /**
+     * if enabled on this page, load the JS + CSS theme.
+     */
+    public function onTwigSiteVariables()
+    {
+        $theme = $this->config->get('plugins.prism-highlight.theme') ?: 'prism-default.css';
+        $this->grav['assets']->addCss('plugin://prism-highlight/css/prism.css');
+        $this->grav['assets']->addCss('plugin://prism-highlight/css/' . $theme);
+        $this->grav['assets']->addJs('plugin://prism-highlight/js/prism.js', null, true, null, 'bottom');
+
+        $inline = '';
+
+        // Always add at least plain text language
+        if ($this->config->get('plugins.prism-highlight.all-pre-blocks')) {
+            $inline .= "$('pre:not([class*=\'language-\'])').addClass('language-txt');\n";
+        }
+
+        // Line Numbers management
+        if ($this->config->get('plugins.prism-highlight.plugins.line-numbers')) {
+            $inline .= "$('pre').addClass('line-numbers');\n";
+        }
+
+        // Command Line management
+        if ($this->config->get('plugins.prism-highlight.plugins.command-line')) {
+            $inline .= "$('pre').addClass('command-line');\n";
+        }
+
+        if ($inline) {
+            $this->grav['assets']->addInlineJs($inline, null, 'bottom');
+        }
+
     }
-
-  }
-
-  /**
-   * if enabled on this page, load the JS + CSS theme.
-   */
-  public function onTwigSiteVariables()
-  {
-    $theme = $this->config->get('plugins.prism-highlight.theme') ?: 'prism-default.css';
-    $this->grav['assets']->addCss('plugin://prism-highlight/css/prism.css');
-    $this->grav['assets']->addCss('plugin://prism-highlight/css/'.$theme);
-    $this->grav['assets']->addJs('plugin://prism-highlight/js/prism.js', null, true, null, 'bottom');
-
-    // Line numbers management
-    if ($this->config->get('plugins.prism-highlight.linenumbers')) {
-      $init = "$('pre').addClass('line-numbers');";
-      $this->grav['assets']->addInlineJs($init, null, 'bottom');
-    }
-  }
 }
